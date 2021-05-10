eb8cf72385dfd2cbe57f6fbfc78e3391ca17cbb9
devramo <omar.djelassi@gmail.com>
2021-05-10 22:39:02 +0200

    minor changes

--- src/views/Workspace.vue
+++ src/views/Workspace.vue
@@ -2,40 +2,46 @@
   <div>
     <h1>WORKSPACE LOGS &amp; SVN SETUP</h1>
     <button @click="selectDirectory($event)">Workspace or Project folder</button>
-    <div class="info">
+    <div class="info" v-show="state.folder">
       <transition-group name="slide-fade">
         <div v-if="state.folder">{{ state.folder }}</div>
-        <div v-if="log4j2Files.length">{{ log4j2Files.length }} log4j2.xml found</div>
-        <div v-if="svnFolders.length" class="svn-folders">
-          SVN {{ svnFolders.length > 1 ? 'Folders:' : 'Folder:' }}
-          <span v-for="(folder, index) in svnFolders" :key="index">
-            {{ basename(folder) }}<span class="sep">,</span>
-          </span>
-        </div>
-      </transition-group>
-      <transition-group name="slide-fade" tag="ul">
-        <li v-if="state.consoleLogsActivated">Console logs activated in all "log4j2.xml" files</li>
       </transition-group>
     </div>
 
     <transition-group name="fade" v-if="state.folder && !state.error">
-      <button @click="toggle($event)" v-if="log4j2Files.length">
-        {{ state.consoleLogsActivated ? 'Desactivate' : 'Activate' }} all console logs <spinner class="spinner" />
+      <button @click="toggle($event)" v-if="state.log4j2Files.length">
+        {{ state.consoleLogsActivated ? 'Desactivate' : 'Activate' }} all ({{ state.log4j2Files.length }}) console logs
+        <spinner class="spinner" />
       </button>
     </transition-group>
+    <div class="info" v-show="state.consoleLogsActivated && state.log4j2Files.length">
+      <transition-group name="slide-fade" tag="ul">
+        <li v-if="state.consoleLogsActivated && state.log4j2Files.length">
+          Console logs activated in all "log4j2.xml" files
+        </li>
+      </transition-group>
+    </div>
 
-    <div v-if="state.folder && !state.error && svnFolders.length">
+    <div v-if="state.folder && !state.error && state.svnFolders.length">
       <div class="info">
         <transition-group name="slide-fade">
           <div class="error" v-if="state.error">{{ state.error }}</div>
-          <div class="error" v-if="!svnClientFound">
+          <div class="error" v-if="!state.svnClientFound">
             SVN client not found<br />Please
             <a href="https://www.collab.net/downloads/subversion" target="_blank">download one here</a>, install it, and
             restart the application.
           </div>
         </transition-group>
+        <transition name="fade">
+          <div v-if="state.svnFolders.length" class="svn-folders">
+            SVN {{ state.svnFolders.length > 1 ? 'Folders:' : 'Folder:' }}
+            <span v-for="(folder, index) in state.svnFolders" :key="index">
+              {{ basename(folder) }}<span class="sep">, </span>
+            </span>
+          </div>
+        </transition>
       </div>
-      <div v-if="svnClientFound">
+      <div v-if="state.svnClientFound && !state.svnIgnoreDone">
         <button
           @click="changeSvnIgnore($event)"
           title="Add log4j2.xml in SVN global ignore list + add target, .project, .classpath, .settings to SVN ignore list"
@@ -43,54 +49,39 @@
           Set SVN ignore list
           <spinner class="spinner" />
         </button>
-        <div class="info">
-          If you get issue when commiting one of the following file or folder: target, .project, .classpath,
-          .settings<br /><br />
-
-          With Eclipse subversion:
-          <ol>
-            <li>Update your project (the working copy) to the head revision.</li>
-            <li>Recreate the file in Eclipse.</li>
-            <li>Set svn:ignore on the file via Team->Add to svn:ignore.</li>
-            <li>Restart eclipse to reflect changes.</li>
-          </ol>
-          With Tortoise SVN:
-          <ol>
-            <li>TortiseSVN -> Delete and add to ignore list</li>
-            <li>Close then re-open the project in Eclipse</li>
-          </ol>
-        </div>
       </div>
     </div>
+    <code v-if="state.svnIgnoreDone && Object.keys(state.changes).length">
+      <pre>{{ state.changes }}</pre>
+    </code>
+    <hr />
+    {{ state.changes }}
   </div>
 </template>
 
 <script>
 import { state } from '@/store/workspace-setup-store'
-import { selectFolder, executeCommand, getStatic } from '@/utils/helper'
+import { selectFolder, executeCommand } from '@/utils/helper'
 import fs from 'fs'
 import path from 'path'
-import { ref } from 'vue'
 import findit from 'findit'
 import Spinner from '../components/Spinner.vue'
 import { onMounted } from 'vue'
+
+const IGNORED_SVN_LIST = ['.settings', 'target', '.project', '.classpath']
 
 export default {
   components: { Spinner },
   name: 'Workspace',
 
   setup() {
-    const svnFolders = ref([])
-    const log4j2Files = ref([])
-    const svnClientFound = ref(null)
-
     function checkSvnClient() {
       executeCommand('svn --version --quiet', null, ({ error }) => {
         if (error) {
-          svnClientFound.value = false
+          state.svnClientFound = false
           return
         }
-        svnClientFound.value = true
+        state.svnClientFound = true
       })
     }
     onMounted(() => {
@@ -109,7 +100,7 @@ export default {
             const dirname = path.basename(dir)
             if (dirname[0] === '.') {
               if (dirname === '.svn') {
-                svnFolders.value.push(path.dirname(dir))
+                state.svnFolders.push(path.dirname(dir))
               }
               stop()
             }
@@ -117,7 +108,7 @@ export default {
           finder.on('file', file => {
             const filename = path.basename(file)
             if (filename === 'log4j2.xml') {
-              log4j2Files.value.push(file)
+              state.log4j2Files.push(file)
               if (state.consoleLogsActivated === null) {
                 fs.readFile(file, 'utf8', function (err, data) {
                   if (err) {
@@ -133,6 +124,7 @@ export default {
               }
             }
           })
+          finder.on('end', () => {})
         }
       }
     }
@@ -145,6 +137,10 @@ export default {
       if (selectedFolder) {
         state.folder = selectedFolder
         state.consoleLogsActivated = null
+        state.svnFolders = []
+        state.log4j2Files = []
+        state.svnIgnoreDone = false
+        state.changes = {}
         existConsoleLogDesactivated()
       }
       button.disabled = false
@@ -154,7 +150,7 @@ export default {
       const button = event.target
       button.disabled = true
 
-      log4j2Files.value.forEach(file => {
+      state.log4j2Files.forEach(file => {
         fs.readFile(file, 'utf8', function (err, data) {
           if (err) {
             return console.log(err)
@@ -184,26 +180,73 @@ export default {
       let button = event.target
       button.disabled = true
       let i = 0
-      svnFolders.value.forEach(svnFolder => {
-        const cmd = `svn propset svn:global-ignores "log4j2.xml" ${svnFolder}`
-        executeCommand(cmd, { cwd: state.folder, env: { ...process.env } }, ({ error }) => {
-          state.error = error
-        })
+      state.svnFolders.forEach(svnFolder => {
+        let cmd = 'svn propget svn:global-ignores'
+        executeCommand(cmd, { cwd: state.folder }, ({ error, output }) => {
+          error && (state.error = error)
 
-        const svnIgnoreFilePath = getStatic('svn-ignore.txt')
-        const cmd2 = `svn propset svn:ignore -F "${svnIgnoreFilePath}" ${svnFolder}`
-        executeCommand(cmd2, { cwd: state.folder, env: { ...process.env } }, ({ error }) => {
-          state.error = error
-          i++
-          if (i >= svnFolders.value.length) {
-            button.disabled = false
+          if (!state.error) {
+            if (output.indexOf('log4j2.xml') == -1) {
+              let cmd = `svn propset svn:global-ignores "log4j2.xml" ${svnFolder}`
+              console.log(cmd)
+              executeCommand(cmd, { cwd: state.folder }, ({ error }) => {
+                if (error) {
+                  state.error = error
+                  return
+                }
+
+                if (!state.changes[basename(svnFolder)]) {
+                  state.changes[basename(svnFolder)] = {}
+                }
+                state.changes[basename(svnFolder)]['global-ignores'] = ['log4j2.xml']
+              })
+            }
           }
         })
+
+        if (!state.error) {
+          cmd = 'svn propget svn:ignore'
+          executeCommand(cmd, { cwd: state.folder }, ({ error, output }) => {
+            error && (state.error = error)
+            if (!state.error) {
+              const ignoredList = output.split(/\s+/)
+              const notAddedYetList = IGNORED_SVN_LIST.filter(v => ignoredList.indexOf(v) === -1)
+              if (notAddedYetList.length) {
+                cmd = `svn propedit svn:ignore ${svnFolder} --editor-cmd "(${notAddedYetList
+                  .map(v => 'echo ' + v)
+                  .join(' & ')}) >>"`
+                console.log(cmd)
+                executeCommand(cmd, { cwd: state.folder }, ({ error }) => {
+                  i++
+                  if (i >= state.svnFolders.length) {
+                    button.disabled = false
+                    state.svnIgnoreDone = true
+                  }
+                  if (error) {
+                    state.error = error
+                    return
+                  }
+                  if (!state.changes[basename(svnFolder)]) {
+                    state.changes[basename(svnFolder)] = {}
+                  }
+                  state.changes[basename(svnFolder)].ignore = notAddedYetList
+                })
+              } else {
+                i++
+                if (i >= state.svnFolders.length) {
+                  button.disabled = false
+                  state.svnIgnoreDone = true
+                }
+              }
+            }
+          })
+        }
+
         console.log(`SVN folder: "${svnFolder}`)
       })
     }
 
-    return { state, selectDirectory, toggle, changeSvnIgnore, svnFolders, log4j2Files, basename, svnClientFound }
+    return { state, selectDirectory, toggle, changeSvnIgnore, basename }
   },
 }
 </script>
